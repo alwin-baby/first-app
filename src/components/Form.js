20cfdc5f783a3bf7b6ee4c999241bf1a5ca3b2e1
Alwin Baby <alwin.baby@diagnal.com>
2022-09-23 11:08:31 +0530

    form functionalities added
    functionalities of obtaining user input and saving it on array has been added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,56 @@
+import { useState, useEffect } from "react";
+
 function Form() {
+    const [input, setInput] = useState("");
+    const [tasks, setTasks] = useState([]);
+
+    //obtaining the value from the input field
+    const handleInput = (e) => {
+        setInput(e.target.value);
+    };
+
+    //adding the values to the tasks array
+    const handleSetTasks = (e) => {
+        setTasks([...tasks, input]);
+    };
+
+    //logging the array for reference
+    useEffect(() => {
+        if (tasks.length) {
+            console.log(tasks);
+        }
+    }, [tasks]);
+
+    //listening for enter key press and calling the handleSetTasks function
+    useEffect(() => {
+        const listener = (event) => {
+            if (event.code === "Enter" || event.code === "NumpadEnter") {
+                event.preventDefault();
+                handleSetTasks();
+            }
+        };
+        document.addEventListener("keydown", listener);
+        return () => {
+            document.removeEventListener("keydown", listener);
+        };
+    });
+
+    //form
     return (
         <div>
-            <form>
-                <div>
-                    <input type="text" placeholder="Add new task" required />
-                </div>
-            </form>
+            <div>
+                <form>
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="Add new task"
+                            value={input}
+                            onChange={handleInput}
+                            required
+                        />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
