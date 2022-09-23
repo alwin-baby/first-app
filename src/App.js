00493d0bd56422c8704e26575e142ebe14614e9b
Alwin Baby <alwin.baby@diagnal.com>
2022-09-23 11:36:36 +0530

    Added no-Task image and text component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
+import NoTaskImage from "./components/NoTaskImage";
 
 function App() {
     return <div>
         <Header />
         <Form />
+        <NoTaskImage />
     </div>;
 }
 
